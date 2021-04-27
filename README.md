e58851336d83a9e0ff5d2141b8b50403606e1606
Daniel Lazar <lazardani0112@gmail.com>
2021-04-27 09:02:45 +0200

    Added type specific paths.

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Fuel Prices In Hungary API
 
 > I made an API for getting the current fuel prices in Hungary what uses the https://hu.fuelo.net/?lang=en website's data.
+
+## Usage
+
+Just go to the https://fuel-prices-hungary-api.vercel.app/ page if you need all data, but you can specify what you're looking for by changing the path. If you don't need all data just use the following paths:
+
+| Path            | Value          |
+| :-------------- | -------------: |
+| /unleaded_95    | Unleaded 95    |
+| /diesel         | Diesel         |
+| /lpg            | LPG            |
+| /unleaded_98    | Unleaded 98    |
+| /diesel_premium | Diesel Premium |
+
+(e.g.: if you only interested in diesel then: https://fuel-prices-hungary-api.vercel.app/diesel)
--- src/index.js
+++ src/index.js
@@ -48,12 +48,44 @@ const fetchData = async () => {
    }
 }
 
+const getType = async (type) => {
+   const data = await fetchData();
+   const { fuelData } = data;
+   dataByType = fuelData.filter(x => x.name === type)[0]
+   return dataByType;
+}
+
 app.get('/',  async (req, res) => {
    const data = await fetchData();
+   res.send(JSON.stringify(data));
+});
+
+app.get('/unleaded_95', async(req, res) => {
+   const data = await getType('Unleaded 95');
+   res.send(JSON.stringify(data));
+});
+
+app.get('/diesel', async(req, res) => {
+   const data = await getType('Diesel');
+   res.send(JSON.stringify(data));
+});
+
+app.get('/lpg', async(req, res) => {
+   const data = await getType('LPG');
+   res.send(JSON.stringify(data));
+});
+
+app.get('/unleaded_98', async(req, res) => {
+   const data = await getType('Unleaded 98');
+   res.send(JSON.stringify(data));
+});
+
+app.get('/diesel_premium', async(req, res) => {
+   const data = await getType('Diesel Premium');
    res.send(JSON.stringify(data));
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
    console.log(`Listening on port ${PORT}`);
-})
+});
